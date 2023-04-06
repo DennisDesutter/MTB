854e5c4ba16ad0dd59cc664c1d02a30009441f21
DennisDesutter <dennis.desutter@ordina.be>
2023-04-06 14:35:20 +0200

    Add downhill as tire type

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -21,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "b6bf38b02b5c6d8dc746ea006246ede5",
 "icons/Icon-maskable-192.png": "b6f229f6546b054363080b4b11679b97",
 "icons/Icon-maskable-512.png": "b6bf38b02b5c6d8dc746ea006246ede5",
-"index.html": "45b8fe80d57c4150667ff84f4aa1a82a",
-"/": "45b8fe80d57c4150667ff84f4aa1a82a",
-"main.dart.js": "e1c101a53be7d99b3d04c614306778ad",
+"index.html": "7968f27c461b472869cce30217abfafb",
+"/": "7968f27c461b472869cce30217abfafb",
+"main.dart.js": "c6694649be73ad18e146c31c1183b868",
 "manifest.json": "d96400cb5138da7ef173e746cf0458a7",
 "version.json": "f531a0b01bc4fc37ca052dbb00141d44"
 };
